b134aa0c1c4b22e6286aebaec0420917c11bad02
bhrgv <bhargav2014duggu@gmail.com>
2016-11-15 15:25:02 +0530

    Sample Test Case Addition

--- fibnocci series/fibnocci series/fibnocci upto given no.cpp
+++ fibnocci series/fibnocci series/fibnocci upto given no.cpp
@@ -1,5 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include<malloc.h>
+#define MAX 20
+#define CASES 4
 /*
 Give proper file names
 fibnocci upto given no -- is not proper naming convention
@@ -10,35 +13,59 @@ dont use spaces and try to give file in one word .. or use general terms like pr
 When giving zero as number of terms . the output is displaying 
 and number of terms printed are less than the given value
 */
-int fibnocci(int n)
+int* fibnocci(int n)
 {
 	int f1, f2,sum,i;
 	f1= 0;
 	f2 = 1;
-	if (n >= 0) 
+	if (n <= 0)
+		return NULL;
+	int* result = (int*)malloc(n*sizeof(int));
+	int index = 0;
+	if (n > 0) 
 	{
-		printf("\n 0");
+		result[index++] = 0;
     }
     if (n >= 1)
 	{
-		printf("\n 1");
+		result[index++] = 1;
 	}
-	if (n>=2)
+	for (i=2;i<n;i++)
 	{
-		for (i=2;i<n;i++)
+		sum = f1 + f2;
+		result[index++] = sum;
+		f1 = f2;
+		f2 = sum;
+	}
+	return result;
+}
+int isCompare(int* actual, int * expected, int n)
+{
+	int flag = 1;
+	for (int i = 0; i < n; i++)
+	{
+		if (actual[i] != expected[i])
 		{
-			sum = f1 + f2;
-			printf("\n %d", sum);
-			f1 = f2;
-			f2 = sum;
+			flag = 0;
+			break;
 		}
 	}
-	return 0;
+	return flag;
+}
+void testcases()
+{
+	int input[CASES] = { 1, 2 ,5 , 7};
+	int output[CASES][MAX] = { { 0 }, { 0, 1 }, { 0, 1, 1, 2, 3, 5 }, { 0, 1, 1, 2, 3, 5 ,8,13} };
+	for (int i = 0; i < CASES; i++)
+	{
+		int* result = fibnocci(input[i]);
+		if (isCompare(result, output[i],input[i]))
+			printf("Passed\n");
+		else
+			printf("Failed\n");
+	}
 }
 void main()
 {
-	int n,s;
-	printf("\n enter the no upto where fibnocci series should be printed");
-	scanf("%d", &n);
-	s = fibnocci(n);
+	testcases();
 }
